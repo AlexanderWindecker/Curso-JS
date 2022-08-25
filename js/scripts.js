27b86bf5e0d341db4caa77df62fd8fcad0ebed93
alexander windecker <waa_alex@hotmail.com>
2022-08-25 10:54:20 -0300

    sumando fetch con json

--- js/scripts.js
+++ js/scripts.js
@@ -1,279 +1,241 @@
-//Sumando DOM
-
-
-const vinos = [{
-        ID: 10,
-        Nombre: "Altos Las Hormigas Gualtallary",
+/* const stockProductos = [{
+        id: 1,
+        nombre: 'Altos Las Hormigas Gualtallary',
+        precio: 261000,
+        imagen: './img/producto0.jpg',
         Uva: "Malbec",
         Ubicacion: "Valle de UCO",
-        Botella: "750ml",
-        Valor: 261000,
+        Stock: 18,
     },
-
     {
-        ID: 20,
-        Nombre: "Kaiken Mai",
+        id: 2,
+        nombre: 'Kaiken Mai',
+        precio: 335000,
+        imagen: './img/producto1.jpg',
         Uva: "Malbec",
         Ubicacion: "Mendoza Argentina",
-        Botella: "750ml",
-        Valor: 335000,
     },
-
     {
-        ID: 30,
-        Nombre: "Altos las Hormigas Reserva",
+        id: 3,
+        nombre: 'Altos las Hormigas Reserva',
+        precio: 160000,
+        imagen: './img/producto2.jpg',
         Uva: "Malbec",
         Ubicacion: "Mendoza Argentina",
-        Botella: "750ml",
-        Valor: 160000,
     },
     {
-        ID: 40,
-        Nombre: "Kaiken Terror Malbec",
-        Uva: "Mixta",
+        id: 4,
+        nombre: 'Kaiken Terror Malbec',
+        precio: 91000,
+        imagen: './img/producto3.jpg',
+        Uva: "Mixto",
         Ubicacion: "Vista Flores-Mendoza",
-        Botella: "750ml",
-        Valor: 91000,
     },
     {
-        ID: 50,
-        Nombre: "Finca el Origen",
+        id: 5,
+        nombre: 'Finca el Origen',
+        precio: 65000,
+        imagen: './img/producto4.jpg',
         Uva: "Malbec",
         Ubicacion: "Mendoza-Valle de Uco",
-        Botella: "750ml",
-        Valor: 66500,
     }
+] */
 
-]
 
+
+const contenedorProductos = document.getElementById('contenedor-productos')
+
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+const botonVaciar = document.getElementById('vaciar-carrito')
+
+const contadorCarrito = document.getElementById('contadorCarrito')
+
+const cantidad = document.getElementById('cantidad')
+const precioTotal = document.getElementById('precioTotal')
+const cantidadTotal = document.getElementById('cantidadTotal')
 
 
 let carrito = []
 
 
-/* let Hola = prompt("Hola gracias por visitarnos, desea ver nuestros catalogo? \n -Si \n -No")
 
-while (Hola != "Si" && Hola != "No") {
-
-    alert("Por favor elija una opcion");
-    Hola = prompt("Hola gracias por visitarnos, desea ver nuestros catalogo? \n -Si \n -No")
-    break;
-}
-
-if (Hola == "Si") {
-
-    alert("Nuestros vinos son:")
-    let nuestrosvinos = vinos.map((catalogo) => "ID: " + catalogo.ID + " - " + catalogo.Nombre + " " + "$" + catalogo.Valor)
-    alert(nuestrosvinos.join(" - "))
-} else if (Hola == "No") {
-    alert("Gracias por visitarnos, vuelva pronto")
-}
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')) {
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
 
 
 
-while (Hola != "No") {
 
+//Alerta libreria
+botonVaciar.addEventListener('click', () => {
 
-    let producto = prompt("Agregue productos a su compra con su ID correspondiente \n ID: 10 - Altos Las Hormigas Gualtallary \n ID: 20 - Kaiken Mai \n ID: 30 - Altos las Hormigas Reserva \n ID: 40 - Kaiken Terror Malbec \n ID: 50 - Finca el Origen ");
-    let Precio = 0
+    swal.fire({
+        title: '¡¡Cuidado!!',
+        text: 'Usted va a vaciar su carrito!',
+        icon: 'warning',
+        iconColor: "red",
+        showCancelButton: true,
 
+    }).then((result) => {
 
-    if (producto == "Altos Las Hormigas Gualtallary" || producto == "Kaiken Mai" ||
-        producto == "Altos las Hormigas Reserva" || producto == "Kaiken Terror Malbec" ||
-        producto == "Finca el Origen") {
-
-        switch (producto) {
-            case "Altos Las Hormigas Gualtallary":
-                Precio = 261000;
-                break;
-            case "Kaiken Mai":
-                Precio = 335000;
-                break;
-            case "Altos las Hormigas Reserva":
-                Precio = 160000;
-                break;
-            case "Kaiken Terror Malbec":
-                Precio = 91000;
-                break;
-            case "Finca el Origen":
-                Precio = 66500;
-                break;
-
-            default:
-                break;
+        if (result.isConfirmed) {
+            carrito.length = 0
+            actualizarCarrito()
+            Swal.fire('Se vacio su carrito')
         }
+    })
+})
 
-        let unidades = parseInt(prompt("Ingrese la cantidad"))
+
+//Alerta libreria
 
 
-        carrito.push({
+//JSON.................................................................................
 
-            producto,
-            unidades,
-            Precio,
+function mostrarProductos() {
+
+    fetch('data.json')
+
+        .then((response) => response.json())
+        .then((stockProductos) => {
+            stockProductos.forEach((producto) => {
+                const div = document.createElement('div')
+                div.classList.add('producto')
+                div.className = "card d-flex col-2 "
+                div.innerHTML = `
+            <img src=${producto.imagen} alt= "" class="card-img-top">
+            <h3>${producto.nombre}</h3>            
+            <p class="precioProducto">Precio:$ ${producto.precio}</p>
+            <button id="agregar${producto.id}" class="boton-agregar bg-danger">Comprar <i class="fas fa-shopping-cart"></i></button>
+            `
+                contenedorProductos.appendChild(div)
+
+
+                const boton = document.getElementById(`agregar${producto.id}`)
+
+                boton.addEventListener('click', () => {
+
+                    agregarAlCarrito(producto.id)
+
+                })
+
+            })
+
         })
-        console.log(carrito);
+
+}
+
+mostrarProductos();
+
+//JSON.................................................................................
+
+
+//asi estaba antes de json.............................................................
+
+/* stockProductos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.className = "card d-flex col-2 "
+    div.innerHTML = `
+    <img src=${producto.imagen} alt= "" class="card-img-top">
+    <h3>${producto.nombre}</h3>
+    
+    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar bg-danger">Comprar <i class="fas fa-shopping-cart"></i></button>
+    `
+    contenedorProductos.appendChild(div)
+
+    const boton = document.getElementById(`agregar${producto.id}`)
+
+    boton.addEventListener('click', () => {
+
+        agregarAlCarrito(producto.id)
+
+    })
+}) */
+
+
+//asi estaba antes de json...........................................................
+
+//AGREGO AL CARRITO
+const agregarAlCarrito = (prodId) => {
+
+    const existe = carrito.some(prod => prod.id === prodId)
+
+    if (existe) {
+        const prod = carrito.map(prod => {
+            if (prod.id === prodId) {
+                prod.cantidad++
+            }
+        })
     } else {
-        alert("No tenemos ese producto")
+        const item = stockProductos.find((prod) => prod.id === prodId)
+        carrito.push(item)
     }
 
+    actualizarCarrito()
+}
 
-    despedida = prompt("Desea algun otro producto? seleccione: \n SI o No");
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
 
+    const indice = carrito.indexOf(item)
 
-    if (despedida === "No") {
+    carrito.splice(indice, 1)
 
-        carrito.forEach((carritoFinal) => {
-            console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar ${carritoFinal.unidades * carritoFinal.Precio}`)
-        })
-        break;
-    }
+    actualizarCarrito()
 
 }
 
+const actualizarCarrito = () => {
 
-const total = carrito.reduce((acc, el) => acc + el.Precio * el.unidades, 0);
+    contenedorCarrito.innerHTML = ""
 
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
 
-let opciones = prompt("El total a pagar es de: $" + total + " quiere abonarlo en cuotas? \n -Si \n -No")
+        contenedorCarrito.appendChild(div)
 
+        localStorage.setItem('carrito', JSON.stringify(carrito))
 
+    })
 
-if (opciones == "No") {
+    contadorCarrito.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad, 0)
 
-    alert("El total de su compra es de $" + total + " Gracias por su compra vuelva pronto");
-
-}
-
-
-
-while (opciones == "Si") {
-
-    let definirCuotas = prompt("Puede hacerlo en: \nB -3 cuotas s/interes, \nC -6 cuotas s/interes,  \nD -12 cuotas s/interes ");
-
-   
-
-    if (definirCuotas == "B" || definirCuotas == "C" || definirCuotas == "D") {
-        switch (definirCuotas) {
-            case "B":
-                cuotas = total / 3;
-               
-               alert  ("Su compra en 3 cuotas le queda en $" + Math.ceil(cuotas) + " C/U");
-                break;
-            case "C":
-                let cuotasSeis = dividir(total, 6);
-
-                alert("Su compra en 6 cuotas le queda en $" + Math.ceil(cuotasSeis) + " C/U");
-                break;
-            case "D":
-                let cuotasDoce = dividir(total, 12);
-
-                alert("Su compra en 12 cuotas le queda en $" + Math.ceil(cuotasDoce) + " C/U");
-                break;
-            default:
-                mensaje = ("Opcion invalida")
-                break;
-        }
-
-    }
-
-    cierre = prompt("Desea corregir las cuotas? seleccione: \n SI o No");
-
-    if (cierre === "No") {
-
-
-        break;
-    }
-
-}
- */
-
-//Eventos
-
-
-function unsaludo () {
-    alert ("Demasiado caro para tu bolsillo")
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
 
 }
 
-const saludo1 = document.getElementById("boton");
+//modal
 
-saludo1.addEventListener("click", unsaludo);
-
-//.............................................
-
-function unsaludo2 () {
-    alert ("No no, este no")
-
-}
-
-const saludo2 = document.getElementById("botonTroll2");
-
-saludo2.addEventListener("click", unsaludo2);
-
-//...............................................
-
-function unsaludo3 () {
-    alert ("Este vino es feo")
-
-}
-
-const saludo3 = document.getElementById("botonTroll3");
-
-saludo3.addEventListener("click", unsaludo3);
-
-//..............................................
-
-function unsaludo4 () {
-    alert ("Te parece este?")
-
-}
-
-const saludo4 = document.getElementById("botonTroll4");
-
-saludo4.addEventListener("click", unsaludo4);
-
-//..............................................
-
-function unsaludo5 () {
-    alert ("Este es el ideal para ti")
-
-}
-
-const saludo5 = document.getElementById("botonTroll5");
-
-saludo5.addEventListener("click", unsaludo5);
-
-//..............................................
-
-//Eventos
+const contenedorModal = document.getElementsByClassName('modal-contenedor')[0]
+const botonAbrir = document.getElementById('boton-carrito')
+const botonCerrar = document.getElementById('carritoCerrar')
+const modalCarrito = document.getElementsByClassName('modal-carrito')[0]
 
 
+botonAbrir.addEventListener('click', () => {
+    contenedorModal.classList.toggle('modal-active')
+})
+botonCerrar.addEventListener('click', () => {
+    contenedorModal.classList.toggle('modal-active')
+})
 
-//DOM
+contenedorModal.addEventListener('click', (event) => {
+    contenedorModal.classList.toggle('modal-active')
 
-
-let mensajecliente = document.createElement("h2");
-mensajecliente.innerHTML = "<h2>Gracias por visitarnos, no vuelva pronto</h2>";
-document.body.append(mensajecliente);
-
-let mensajecliente1 = document.createElement("h2");
-mensajecliente1.innerHTML = "<h2>Va en realidad regrese y riase un rato</h2>";
-document.body.append(mensajecliente1);
-
-let mensajecliente2 = document.createElement("h2");
-mensajecliente2.innerHTML = "<h2>Total no va a comprar nada si esta todo re caro jajajaja</h2>";
-document.body.append(mensajecliente2);
-
-
-//DOM
-
-
-
-function dividir(a, b) {
-    return a / b;
-}
-
-function multiplicar(a, b) {
-    return a * b;
-}
+})
+modalCarrito.addEventListener('click', (event) => {
+    event.stopPropagation()
+})
